104a462484e65e5a5d2d87b903138415746d5bf5
Julian Toscani <toscani@cekom.de>
2021-08-25 15:25:43 +0200

    add layout components

---
+++ src/components/layout/Footer.vue
@@ -0,0 +1,31 @@
+<template>
+  <footer>
+    <img src="/imgs/logo.svg" alt="" />
+    <address>
+      <ul>
+        <li>
+          <span>Email: </span>
+          <a href="mailto:me@you.com"> me@you.com </a>
+        </li>
+        <li>
+          <span> Owner: </span>
+          <span> Carl Heinz </span>
+        </li>
+        <li>
+          <span>Tel:</span>
+          <a href="tel:+555 445566"> +555 445566 </a>
+        </li>
+      </ul>
+    </address>
+  </footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Footer",
+});
+</script>
+
+<style></style>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+    <h1>Hello!</h1>
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/about"> About </router-link>
+      <router-link to="/profile"> Profile </router-link>
+    </nav>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Header",
+});
+</script>
+
+<style></style>
